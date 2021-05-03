bd8b776da44e042ce0f47d55129676308ba15f1d
husnutass <husnutas@outlook.com>
2021-05-03 09:05:59 +0300

    email, telefon pattern ayristirmasi yapildi

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:email_validator/email_validator.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -13,11 +14,24 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   File _image;
   final picker = ImagePicker();
+  List<String> email = [];
+  List<String> phone = [];
+  List<String> address = [];
+
+  bool isNumeric(String s) {
+    if (s == null) {
+      return false;
+    }
+    return double.tryParse(s) != null && s.length >= 10;
+  }
 
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
+      email = [];
+      phone = [];
+      address = [];
       _image = File(pickedFile.path);
       final FirebaseVisionImage visionImage =
           FirebaseVisionImage.fromFile(_image);
@@ -35,18 +49,41 @@ class _HomeState extends State<Home> {
         print("cornerPoints: " + cornerPoints.toString());
         final String text = block.text;
         print("text: " + text);
+
         final List<RecognizedLanguage> languages = block.recognizedLanguages;
         // print("languages: " + languages.toString());
 
         for (TextLine line in block.lines) {
           // Same getters as TextBlock
-          // print("line: " + line.toString());
+          print("line: " + line.text);
+          if (EmailValidator.validate(line.text)) {
+            if (phone != null) {
+              setState(() {
+                email.add(line.text);
+              });
+            }
+          } else if (isNumeric(line.text.replaceAll(' ', ''))) {
+            if (phone != null) {
+              setState(() {
+                phone.add(line.text);
+              });
+            }
+          } else {
+            if (address != null) {
+              setState(() {
+                address.add(line.text);
+              });
+            }
+          }
           for (TextElement element in line.elements) {
             // Same getters as TextBlock
             // print("element: " + element.toString());
           }
         }
       }
+
+      print("email: " + email.toString());
+      print("phone: " + phone.toString());
     } else {
       print('No image selected.');
     }
@@ -54,11 +91,50 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FloatingActionButton(
+    return Column(children: [
+      createRow("Email", email),
+      SizedBox(
+        height: 30,
+      ),
+      createRow("Telefon", phone),
+      SizedBox(
+        height: 30,
+      ),
+      createRow("Adres", address),
+      SizedBox(
+        height: 30,
+      ),
+      FloatingActionButton(
         onPressed: getImage,
         child: Icon(Icons.photo),
       ),
+    ]);
+  }
+
+  Row createRow(String name, List<String> list) {
+    if (list.length > 0) {
+      return Row(
+        children: [
+          Text("$name: ", style: TextStyle(fontSize: 20)),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Text(list[index], style: TextStyle(fontSize: 20));
+              },
+            ),
+          ),
+        ],
+      );
+    }
+    return Row(
+      children: [
+        Text(
+          "$name boş",
+          style: TextStyle(fontSize: 20),
+        )
+      ],
     );
   }
 }
