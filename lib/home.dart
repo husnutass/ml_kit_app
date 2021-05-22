db32e382ae9d8ee0fa1e97ad6b14ad257d1c1b28
husnutass <husnutas@outlook.com>
2021-05-22 12:02:41 +0300

    regex patternler eklendi

--- lib/home.dart
+++ lib/home.dart
@@ -29,6 +29,73 @@ class _HomeState extends State<Home> {
     return double.tryParse(s) != null && s.length >= 10;
   }
 
+  String getWebSite(url) {
+    RegExp regExp = new RegExp(
+      r"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)",
+      caseSensitive: false,
+      multiLine: false,
+    );
+
+    print("hasMatch : " + regExp.hasMatch(url).toString());
+    print("stringMatch : " + regExp.stringMatch(url).toString());
+
+    if (regExp.hasMatch(url)) {
+      return regExp.stringMatch(url).toString();
+    }
+    return null;
+  }
+
+  String getPhoneNumber(phone) {
+    RegExp regExp = new RegExp(
+      r"(([\+]90?)|([0]?))([ ]?)((\([0-9]{3}\))|([0-9]{3}))([ ]?)([0-9]{3})(\s*[\-]?)([0-9]{2})(\s*[\-]?)([0-9]{2})",
+      caseSensitive: false,
+      multiLine: false,
+    );
+
+    print("hasMatch : " + regExp.hasMatch(phone).toString());
+    print("stringMatch : " + regExp.stringMatch(phone).toString());
+
+    if (regExp.hasMatch(phone)) {
+      return regExp.stringMatch(phone).toString();
+    }
+    return null;
+  }
+
+  /// returns if the given phone number is mobile or business
+  String checkPhoneNumberType(phone) {
+    if (phone[0] == "0") {
+      if (phone[1] == "(") {
+        if (phone[2] == "5") {
+          return "mobile";
+        } else {
+          return "business";
+        }
+      } else {
+        if (phone[1] == "5") {
+          return "mobile";
+        } else {
+          return "business";
+        }
+      }
+    } else if (phone[0] == "(") {
+      if (phone[1] == "0") {
+        if (phone[2] == "5") {
+          return "mobile";
+        } else {
+          return "business";
+        }
+      } else if (phone[1] == "5") {
+        return "mobile";
+      } else {
+        return "business";
+      }
+    } else if (phone[0] == "5") {
+      return "mobile";
+    } else {
+      return "business";
+    }
+  }
+
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
 
@@ -42,22 +109,27 @@ class _HomeState extends State<Home> {
           FirebaseVisionImage.fromFile(_image);
       final TextRecognizer textRecognizer =
           FirebaseVision.instance.textRecognizer();
+      // final TextRecognizer cloudTextRecognizer = FirebaseVision.instance
+      //     .cloudTextRecognizer(CloudTextRecognizerOptions(
+      //         textModelType: CloudTextModelType.dense,
+      //         hintedLanguages: ["tr"]));
       final VisionText visionText =
           await textRecognizer.processImage(visionImage);
 
-      print(visionText.text);
+      // print(visionText.text);
 
       var requestData = [];
+      var requestData2 = [];
 
       for (TextBlock block in visionText.blocks) {
         final Rect boundingBox = block.boundingBox;
         final String area =
             (boundingBox.size.width * boundingBox.size.height).toString();
-        print("boundingBoxArea: " + area);
+        // print("boundingBoxArea: " + area);
         final List<Offset> cornerPoints = block.cornerPoints;
-        print("cornerPoints: " + cornerPoints.toString());
+        // print("cornerPoints: " + cornerPoints.toString());
         final String text = block.text;
-        print("text: " + text);
+        // print("text: " + text);
 
         var ocrData = {
           "text": text ?? "",
@@ -67,32 +139,28 @@ class _HomeState extends State<Home> {
         };
 
         requestData.add(ocrData);
-        final List<RecognizedLanguage> languages = block.recognizedLanguages;
+        // final List<RecognizedLanguage> languages = block.recognizedLanguages;
         // print("languages: " + languages.toString());
-        /*
 
         for (TextLine line in block.lines) {
           // Same getters as TextBlock
-          print("line: " + line.text);
+          // print("line: " + line.text);
           if (EmailValidator.validate(line.text)) {
-            if (phone != null) {
+            if (email != null) {
               setState(() {
                 email.add(line.text);
               });
             }
-          } else if (isNumeric(line.text.replaceAll(
-              // RegExp(r"\b(\w*tel\w*)|(\w*gsm\w*)\b|(\s)", caseSensitive: false),
-              ' ',
-              ''))) {
+          } else if (getPhoneNumber(line.text.replaceAll(' ', '')) != null) {
             if (phone != null) {
               setState(() {
-                phone.add(line.text);
+                phone.add(getPhoneNumber(line.text.replaceAll(' ', '')));
               });
             }
-          } else if (line.text.contains("www")) {
+          } else if (getWebSite(line.text) != null) {
             if (site != null) {
               setState(() {
-                site.add(line.text);
+                site.add(getWebSite(line.text));
               });
             }
           } else {
@@ -102,14 +170,38 @@ class _HomeState extends State<Home> {
               });
             }
           }
-          for (TextElement element in line.elements) {
-            // Same getters as TextBlock
-            // print("element: " + element.toString());
-          }
+          final String lineArea =
+              (line.boundingBox.size.width * line.boundingBox.size.height)
+                  .toString();
+          var ocrData = {
+            "text": line.text ?? "",
+            "boundingBoxArea": lineArea ?? "",
+            "cornerPointsDx": line.cornerPoints[0]?.dx.toString() ?? "",
+            "cornerPointsDy": line.cornerPoints[0]?.dy.toString() ?? "",
+          };
+
+          requestData2.add(ocrData);
+
+          // for (TextElement element in line.elements) {
+          //   // Same getters as TextBlock
+          //   // print("element: " + element.toString());
+          //   final String elementArea = (element.boundingBox.size.width *
+          //           element.boundingBox.size.height)
+          //       .toString();
+          //   var ocrData = {
+          //     "text": element.text ?? "",
+          //     "boundingBoxArea": elementArea ?? "",
+          //     "cornerPointsDx": element.cornerPoints[0]?.dx.toString() ?? "",
+          //     "cornerPointsDy": element.cornerPoints[0]?.dy.toString() ?? "",
+          //   };
+
+          //   requestData3.add(ocrData);
+          // }
         }
-        */
       }
-      writeToCsv(requestData);
+      // writeToCsv(requestData);
+      print("blockData: " + requestData.toString());
+      print("lineData: " + requestData2.toString());
     } else {
       print('No image selected.');
     }
@@ -117,7 +209,7 @@ class _HomeState extends State<Home> {
 
   Future<http.Response> writeToCsv(List data) {
     return http.post(
-      Uri.http("192.168.1.36:4444", "/api/dataset"),
+      Uri.http("192.168.1.5:4444", "/api/dataset"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
